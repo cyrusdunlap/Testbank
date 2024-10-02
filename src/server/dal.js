a52a1adb67e111157fefefaa29718fe54ef1c1e4
cyrusdunlap <155677876+cyrusdunlap@users.noreply.github.com>
2024-10-02 13:52:27 -0500

    test 16

--- src/server/dal.js
+++ src/server/dal.js
@@ -1,49 +1,49 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const url = "mongodb://localhost:27017";
-// // let db = null;
-
-// //connect to mongo
-// MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-//   console.log("Connected successfully to db server");
-
-//   // connect to myproject database
-//   db = client.db("myproject");
-// });
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
+const MongoClient = require("mongodb").MongoClient;
+const url =
   "mongodb+srv://cdunlap09:P8pyEaWZwoC9n2SF@cluster0.qa5xi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+let db = null;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+//connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
+  console.log("Connected successfully to db server");
+
+  // connect to myproject database
+  db = client.db("myproject");
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("myproject").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri =
+//   "mongodb+srv://cdunlap09:P8pyEaWZwoC9n2SF@cluster0.qa5xi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-db = client.db("my project");
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("myproject").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// db = client.db("my project");
 
 // create user account
 function create(name, email, password) {
-  // db = client.db("my project");
   return new Promise((resolve, reject) => {
     const collection = db.collection("users");
     const doc = { name, email, password, balance: 0 };
